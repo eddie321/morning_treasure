1197939b2d235167d079567b1890b1585a77fda1
Ronan <ronan.dineen@webinnovate.ie>
2017-09-29 14:32:07 +0200

    list.php changes

--- list.php
+++ list.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once 'db.php';
+include_once 'db.php';
+require_once 'functions.php';
+require_once 'availability.php';
 require_once 'manufacturers.php';
 require_once 'product_category.php';
 $db = db_connect();
@@ -8,25 +10,27 @@ $db = db_connect();
 
 $stmt = $db->prepare('SELECT * FROM golf_gear');
 $stmt->execute();
-$movies = $stmt->fetchALL();
+$golf_gears = $stmt->fetchALL();
 
-// var_dump($movies);
+// var_dump($golf_gear);
 
 
 
 foreach ($golf_gears as $golf_gear) {
     echo htmlspecialchars($golf_gear['name']);
     echo('<br>');
-    echo htmlspecialchars($golf_gear['description']);
-    echo('<br>');
     echo htmlspecialchars($golf_gear['price']);
-    echo('<br>');
-    echo '<strong>Product Category:';
-    echo htmlspecialchars($golf_gears[$golf_gear['product_category']]);
-    echo '</strong>';
     echo('<br>');
     echo '<strong>Manufacturer:';
     echo htmlspecialchars($golf_gears[$golf_gear['manufacturer']]);
+    echo '</strong>';
+    echo('<br>');
+    echo htmlspecialchars($golf_gear['description']);
+    echo('<br>');
+    echo htmlspecialchars($golf_gear['availability']);
+    echo('<br>');
+    echo '<strong>Product Category:';
+    echo htmlspecialchars($golf_gears[$golf_gear['product_category']]);
     echo '</strong>';
     echo('<br>');
     echo '<a href="edit.php?id=' . htmlspecialchars($golf_gear['id']) . '">edit</a>';
