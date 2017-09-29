167e3dcd2582178e03a1fa7787b0ce1ce046f6de
YudhaYuki <Yudha.Yuki@hotmail.com>
2017-09-29 13:37:34 +0200

    adding db.php

--- db.php
+++ db.php
@@ -1,9 +1,8 @@
 <?php
 
-require_once 'config.php';
-
 function db_connect()
 {
+include_once 'config.php';
 $db = new PDO("mysql:host=localhost;dbname=$db_name;charset=utf8", $db_user,$db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//enabling errors just for our case
 return $db;
