91e388669657f4e95aa204192d9e8a37f3ae3f5d
eddie <edwina.landale@live.com.au>
2017-09-29 11:15:51 +0200

    fixed names again

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 function db_connect()
 {
-$db = new PDO('mysql:host=localhost;dbname=golf_gear;charset=utf8', 'root','root');
+$db = new PDO('mysql:host=localhost;dbname=golf_gear;charset=utf8', 'root','Mundiwa1995');
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//enabling errors just for our case
 return $db;
 }
