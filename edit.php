c9cbf07ae2620bb4f4551d10c3e2ab7e1a6ef1b2
YudhaYuki <Yudha.Yuki@hotmail.com>
2017-09-29 15:13:59 +0200

    edit.php is fixed

--- edit.php
+++ edit.php
@@ -4,12 +4,17 @@ require_once 'functions.php';
 require_once 'availability.php';
 require_once 'manufacturers.php';
 require_once 'product_category.php';
+
 $db = db_connect();
 
+if (!isset($_GET['id'])) {
+    echo 'no id';  // 
+    exit;
+}
+
+
 $stmt = $db->prepare('SELECT * FROM golf_gear WHERE id = ?');
-$stmt-> execute([$_GET['id']]);
+$stmt->execute([$_GET['id']]);
 $golf_gear = $stmt->fetch();
 
-var_dump($golf_gear);
-
-echo build_form($golf_gear['name'], $golf_gear['price'], $golf_gear['manufacturer'], $golf_gear['product_description'], $golf_gear['availability'], $golf_gear['product_category']);
+echo build_form($golf_gear['name'], $golf_gear['product_price'], $golf_gear['manufacturer'], $golf_gear['product_description'], $golf_gear['availability'], $golf_gear['product_category']);
