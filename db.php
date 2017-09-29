dfc284c911d2e980bea9f05c97d8c9232af28d87
YudhaYuki <Yudha.Yuki@hotmail.com>
2017-09-29 13:25:45 +0200

    changed stuff in db.php

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ require_once 'config.php';
 
 function db_connect()
 {
-$db = new PDO('mysql:host=localhost;dbname=$db_name;charset=utf8', $db_user,$db_pass);
+$db = new PDO("mysql:host=localhost;dbname=$db_name;charset=utf8", $db_user,$db_pass);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//enabling errors just for our case
 return $db;
 }
